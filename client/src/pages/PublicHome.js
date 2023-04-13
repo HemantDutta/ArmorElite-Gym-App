22ca546a98d1377f5f31d93a20f845f3c08aa4c2
HemantDutta <hemantnorddutta2@gmail.com>
2023-04-13 17:24:59 +0530

    Add Hero Text and Icon

--- client/src/pages/PublicHome.js
+++ client/src/pages/PublicHome.js
@@ -6,6 +6,7 @@ export const PublicHome = () => {
     setInterval(changeHero, 5000);
     function changeHero()
     {
+        let currID;
         let hI1 = document.getElementById("hI1");
         let hI2 = document.getElementById("hI2");
         let hI3 = document.getElementById("hI3");
@@ -14,27 +15,25 @@ export const PublicHome = () => {
         if(hI1.classList.contains("hero-img-active"))
         {
             hI1.classList.remove("hero-img-active");
-            hI2.classList.add("hero-img-active");
-            return null;
+            currID = "hI2";
         }
-        else if(hI2.classList.contains("hero-img-active"))
+        if(hI2.classList.contains("hero-img-active"))
         {
             hI2.classList.remove("hero-img-active");
-            hI3.classList.add("hero-img-active");
-            return null;
+            currID = "hI3";
         }
-        else if(hI3.classList.contains("hero-img-active"))
+        if(hI3.classList.contains("hero-img-active"))
         {
             hI3.classList.remove("hero-img-active");
-            hI4.classList.add("hero-img-active");
-            return null;
+            currID = "hI4";
         }
-        else if(hI4.classList.contains("hero-img-active"))
+        if(hI4.classList.contains("hero-img-active"))
         {
             hI4.classList.remove("hero-img-active");
-            hI1.classList.add("hero-img-active");
-            return null;
+            currID = "hI1";
         }
+
+        document.getElementById(currID).classList.add("hero-img-active");
     }
 
     return (
@@ -47,23 +46,25 @@ export const PublicHome = () => {
                 <div className="hero" id="hero">
                     <div className="hero-container">
                         <div className="hero-slide-container hero-img-1 hero-img-active" id="hI1">
-                            <img src="assets/images/hero_img_1.jpg" alt="hero-slide-image"/>
+                            <img src="assets/images/hero_img_1.jpg" alt="hero-slide"/>
                         </div>
                         <div className="hero-slide-container hero-img-2" id="hI2">
-                            <img src="assets/images/hero_img_2.jpg" alt="hero-slide-image"/>
+                            <img src="assets/images/hero_img_2.jpg" alt="hero-slide"/>
                         </div>
                         <div className="hero-slide-container hero-img-3" id="hI3">
-                            <img src="assets/images/hero_img_3.jpg" alt="hero-slide-image"/>
+                            <img src="assets/images/hero_img_3.jpg" alt="hero-slide"/>
                         </div>
                         <div className="hero-slide-container hero-img-4" id="hI4">
-                            <img src="assets/images/hero_img_4.jpg" alt="hero-slide-image"/>
+                            <img src="assets/images/hero_img_4.jpg" alt="hero-slide"/>
                         </div>
                         <div className="hero-overlay-container"/>
                         <div className="hero-text-container">
                             <div className="hero-text-header">
-                                <span></span>
+                                <span>Unleash Your Being</span>
                             </div>
-                            <div className="hero-text-description"></div>
+                            <div className="hero-text-description">
+                                <span>Transform Yourself Into The</span><span>Greatest Version Of Yourself</span>
+                            </div>
                             <div className="hero-text-icon">
                                 <img src="assets/images/scrollDown.png" alt="Scroll Down"/>
                             </div>
