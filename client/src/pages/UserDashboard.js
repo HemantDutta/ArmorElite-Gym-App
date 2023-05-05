f72b96d900b4af9110760c2aa881635a0459494d
HemantDutta <hemantnorddutta2@gmail.com>
2023-05-05 15:13:44 +0530

    Create User Dashboard HTML Structure

--- client/src/pages/UserDashboard.js
+++ client/src/pages/UserDashboard.js
@@ -1,7 +1,26 @@
 export const UserDashboard = () => {
     return (
         <>
-
+            <div className="userDashboard-container">
+                <div className="userDashboard">
+                    <div className="userDashboard-topBar">
+                        <div className="topBar-leftSection-container"></div>
+                        <div className="topBar-rightSection-container"></div>
+                    </div>
+                    <div className="userDashboard-userProfile-container">
+                        <div className="userProfile-left-container"></div>
+                        <div className="userProfile-right-container"></div>
+                    </div>
+                    <div className="userDashboard-workout-container">
+                        <div className="workout-header-container"></div>
+                        <div className="workout-optionGrid-container"></div>
+                    </div>
+                    <div className="userDashboard-profileOption-container">
+                        <div className="profileOption-header-container"></div>
+                        <div className="profileOption-optionGrid-container"></div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
