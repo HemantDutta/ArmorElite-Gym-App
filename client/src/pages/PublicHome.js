7feba8aab5ea94f452f4eb91738ffd87c6c965e7
HemantDutta <hemantnorddutta2@gmail.com>
2023-06-06 16:31:08 +0530

    Remove Hero Carousel

--- client/src/pages/PublicHome.js
+++ client/src/pages/PublicHome.js
@@ -122,51 +122,10 @@ export const PublicHome = () => {
     //Navigator
     const nav = useNavigate();
 
-    //Hero Functions
-    const heroInterval = setInterval(changeHero, 5000);
-
-    function changeHero() {
-        try {
-            let currID;
-            let hI1 = document.getElementById("hI1");
-            let hI2 = document.getElementById("hI2");
-            let hI3 = document.getElementById("hI3");
-            let hI4 = document.getElementById("hI4");
-
-            if (hI1.classList.contains("hero-img-active")) {
-                hI1.classList.remove("hero-img-active");
-                currID = "hI2";
-            }
-            if (hI2.classList.contains("hero-img-active")) {
-                hI2.classList.remove("hero-img-active");
-                currID = "hI3";
-            }
-            if (hI3.classList.contains("hero-img-active")) {
-                hI3.classList.remove("hero-img-active");
-                currID = "hI4";
-            }
-            if (hI4.classList.contains("hero-img-active")) {
-                hI4.classList.remove("hero-img-active");
-                currID = "hI1";
-            }
-            document.getElementById(currID).classList.add("hero-img-active");
-        } catch (e) {
-            console.log(e);
-        }
-    }
-
     //Navigate To Packages
     function showPackages() {
-        clearInterval(heroInterval);
         nav("/packages");
     }
-
-    //Mounted or Not
-    useEffect(() => {
-        return () => {
-            clearInterval(heroInterval);
-        };
-    }, []);
 
 
     return (
@@ -180,15 +139,6 @@ export const PublicHome = () => {
                     <div className="hero-container">
                         <div className="hero-slide-container hero-img-1 hero-img-active" id="hI1">
                             <img src="assets/images/hero_img_1.jpg" alt="hero-slide"/>
-                        </div>
-                        <div className="hero-slide-container hero-img-2" id="hI2">
-                            <img src="assets/images/hero_img_2.jpg" alt="hero-slide"/>
-                        </div>
-                        <div className="hero-slide-container hero-img-3" id="hI3">
-                            <img src="assets/images/hero_img_3.jpg" alt="hero-slide"/>
-                        </div>
-                        <div className="hero-slide-container hero-img-4" id="hI4">
-                            <img src="assets/images/hero_img_4.jpg" alt="hero-slide"/>
                         </div>
                         <div className="hero-overlay-container"/>
                         <div className="hero-text-container">
